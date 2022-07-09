64f23b54fdb61beb8ea9016edd77d96ef845600b
franknmungai <frankmnganga@gmail.com>
2022-07-09 12:17:21 +0300

    showcase project animations

--- components/ShowCase.tsx
+++ components/ShowCase.tsx
@@ -8,6 +8,12 @@ enum Tabs {
   Mindblowing = 'Mindblowing',
 }
 
+const sampleProjects = {
+  [Tabs.Favorite]: ['1', '2', '3', '4', '17', '18'],
+  [Tabs.Hot]: ['5', '6', '7', '8', '19', '20'],
+  [Tabs.Amazing]: ['9', '10', '11', '12', '21', '22'],
+  [Tabs.Mindblowing]: ['13', '14', '15', '16', '23', '24'],
+};
 const ShowCase = () => {
   const [activeTab, setActiveTab] = useState<Tabs>(Tabs.Favorite);
 
@@ -24,14 +30,36 @@ const ShowCase = () => {
 
   return (
     <div className={styles.container}>
+      {/* Tabs */}
       <div className={styles.tabs}>
-        {/* Tabs */}
         {tabsContent.map(([name, icon]) => {
           return (
             <span
-              onClick={() => setActive(name as Tabs)}
+              key={name}
               className={`${isActive(name as Tabs) && styles.active}`}
-            >{`${name} ${icon}`}</span>
+              onClick={setActive.bind(this, name as Tabs)}
+            >
+              {`${name}`} <span className={styles.tab_icon}>{` ${icon}`}</span>
+            </span>
+          );
+        })}
+      </div>
+
+      {/* Projects */}
+
+      <div className={styles.projects}>
+        {/* <div className={styles.piece}></div>
+        <div className={styles.piece}></div>
+        <div className={styles.piece}></div>
+        <div className={styles.piece}></div>
+        <div className={styles.piece}></div>
+        <div className={styles.piece}></div> */}
+
+        {sampleProjects[activeTab].map((project) => {
+          return (
+            <div className={styles.piece} key={project}>
+              {project}
+            </div>
           );
         })}
       </div>
