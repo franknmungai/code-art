c157896cae3a3c3a367bf6cb8ec17654690c3104
franknmungai <frankmnganga@gmail.com>
2022-07-16 20:11:12 +0300

    production ready

--- apollo-client.ts
+++ apollo-client.ts
@@ -1,18 +1,19 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
-import process from 'process';
+import { STEPZEN_API_KEY } from './utils/env';
 
 // Production
-// const client = new ApolloClient({
-//   uri: 'https://southlake.stepzen.net/api/canvas-art/__graphql',
-//   headers: {
-//     Authorization: ('Apikey ' + process.env?.STEPZEN_API_KEY) as string,
-//   },
-//   cache: new InMemoryCache(),
-// });
-
 const client = new ApolloClient({
-  uri: 'http://localhost:5001/api/canvas-art',
+  uri: 'https://southlake.stepzen.net/api/canvas-art/__graphql',
+  headers: {
+    Authorization: ('Apikey ' +
+      STEPZEN_API_KEY) /*process.env?.STEPZEN_API_KEY*/ as string,
+  },
   cache: new InMemoryCache(),
 });
+
+// const client = new ApolloClient({
+//   uri: 'http://localhost:5001/api/canvas-art',
+//   cache: new InMemoryCache(),
+// });
 
 export default client;
